908912dbf52a4a9db770faf43cf2e6888ceb72f1
mitchellshera <mitchellwachera@gmail.com>
2023-06-26 15:51:26 +0300

    111

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,35 +8,40 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+    listint_t *node;
 
-    listint_t *curr = (*list)->next;
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		while ((node->prev) && (node->prev->n > node->n))
+		{
+			node = swap_node(node, list);
+			print_list(*list);
+		}
+		node = node->next;
+	}
+}
+/**
+ *swap_node - swaps a node for his previous one
+ *@node: node
+ *@list: node list
+ *Return: return a pointer to a node which was enter it
+ */
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *curr = node;
 
-    while (curr != NULL)
-    {
-        listint_t *key = curr;
-        listint_t *prev = key->prev;
-
-        while (prev != NULL && prev->n > key->n)
-        {
-            prev->next = key->next;
-            if (key->next != NULL)
-                key->next->prev = prev;
-
-            key->prev = prev->prev;
-            key->next = prev;
-
-            if (prev->prev != NULL)
-                prev->prev->next = key;
-            else
-                *list = key;
-
-            prev->prev = key;
-
-            prev = key->prev;
-        }
-
-        curr = curr->next;
-    }
+	back->next = curr->next;
+	if (curr->next)
+		curr->next->prev = back;
+	curr->next = back;
+	curr->prev = back->prev;
+	back->prev = curr;
+	if (curr->prev)
+		curr->prev->next = curr;
+	else
+		*list = curr;
+	return (curr);
 }
